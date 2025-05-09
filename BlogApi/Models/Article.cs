46afbfa31cd862defd7441745e26136b5c53d4cd
Logan <lodev205@gmail.com>
2025-05-09 17:44:32 -0500

    🧱 Add data annotations to `Article` class
    This commit adds data annotations to the Article class to define the database schema for EF Core Code First migrations.
    
    These annotations provide instructions to EF Core on how to create the database schema, ensuring consistency between the model and the database. They do not alter application behavior.

--- BlogApi/Models/Article.cs
+++ BlogApi/Models/Article.cs
@@ -1,18 +1,40 @@
-﻿namespace BlogApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApi.Models
 {
     public class Article
     {
+        [Key]
+        [Column("id", TypeName = "int")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [Column("title", TypeName = "text")]
         public string Title { get; set; } = string.Empty!;
+
+        [Required]
+        [Column("content", TypeName = "text")]
         public string Content { get; set; } = string.Empty!;
+
+        [Required]
+        [Column("author", TypeName = "text")]
         public string Author { get; set; } = string.Empty!;
+
+        [Column("tags", TypeName = "text")]
         public string Tags { get; set; } = string.Empty!;
-        public DateTime CreatedAt { get; set; } 
+
+        [Column("created_at", TypeName = "text")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at", TypeName = "text")]
         public DateTime UpdatedAt { get; set; }
 
         public Article()
         {
             CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
         }
 
     }
