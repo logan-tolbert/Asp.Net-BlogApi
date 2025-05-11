768be2f8670b51bcaa4108c429edf27860c42264
Logan <lodev205@gmail.com>
2025-05-11 08:01:42 -0500

    🧪 Add integration tests for ArticleRepo and helper
    This commit introduces `DbContextHelper.cs` used to centralize in-memory `DbContext` configuration for consistent testing. Also, .initial ntegration tests for the `ArticleRepo` have been implemented. Adding `IT_ArticleRepoTest.cs` with initial passing tests:
     - `GetByIdAsync_ReturnsCorrectArticle_WhenExists()`
     - `GetByIdAsync_ReturnsNull_WhenNotExists()`
    
    A `DbContextHelper.cs` is was also  .

---
+++ BlogApi.Tests/IntegrationTests/IT_ArticleRepoTest.cs
@@ -0,0 +1,46 @@
+﻿using BlogApi.DataAccess.Repository;
+using BlogApi.Tests.TestHelpers;
+
+namespace BlogApi.Tests.IntegrationTests;
+public class IT_ArticleRepoTest
+{
+    // *-- Create --*
+
+    // *-- Read --* 
+    [Fact]
+    public async Task GetByIdAsync_ReturnsCorrectArticle_WhenExists()
+    {
+        // Arrange
+        await using var ctx = await DbContextHelper.ConfigureTestContext();
+        var repo = new ArticleRepo(ctx);
+        const int id = 1;
+        
+        // Act
+        var result = await repo.GetByIdAsync(id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(id, result.Id);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ReturnsNull_WhenNotExists()
+    {
+        // Arrange 
+        await using var ctx = await DbContextHelper.ConfigureTestContext();
+        var repo = new ArticleRepo(ctx);
+
+        // Act
+        var result = await repo.GetByIdAsync(999);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+
+    // *-- Update --*
+
+    // *-- Delete --*
+}
+
+
