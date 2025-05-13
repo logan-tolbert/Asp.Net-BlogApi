505c77f9c1171d8f3265a9fa93f7625ffeee3a47
Logan <lodev205@gmail.com>
2025-05-13 06:13:08 -0500

    ✨ feat: Add standardized error responses using Problem Details
    This commit improves API error handling by returning consistent, machine-readable error responses based on the Problem Details specification.

--- Blog.Api/Program.cs
+++ Blog.Api/Program.cs
@@ -1,12 +1,15 @@
 using DataAccess;
 using DataAccess.Repository;
-using Microsoft.EntityFrameworkCore;
 using Blog.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
 
+// Database
 builder.Services.AddDbContext<BlogDbContext>();
 
 builder.Services.AddScoped<IArticleRepo, ArticleRepo>();
@@ -14,10 +17,44 @@ builder.Services.AddScoped<IArticleService, ArticleService>();
 
 var app = builder.Build();
 
+app.UseExceptionHandler();
+
+// Configures UseStatusCodePages to re-execute the pipeline and generate Problem Details
+app.UseStatusCodePages(async statusCodeContext =>
+{
+    // TODO: Refactor Problem Details setup into a dedicated ProblemDetailsService for better organization.
+    var problemDetailsService = statusCodeContext.HttpContext.RequestServices.GetRequiredService<IProblemDetailsService>();
+
+    var httpContext = statusCodeContext.HttpContext;
+    var request = httpContext.Request;
+    var response = httpContext.Response;
+
+    if (!response.HasStarted && response.StatusCode >= 400)
+    {
+        await problemDetailsService.WriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            ProblemDetails = new ProblemDetails
+            {
+                Status = response.StatusCode,
+                Title = ReasonPhrases.GetReasonPhrase(response.StatusCode),
+                Instance = request.Path
+            }
+        });
+    }
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
 app.MapControllers();
 
 app.Run();
 
+
+//  *-- For testing --*
 public abstract partial class Program
 {
 
